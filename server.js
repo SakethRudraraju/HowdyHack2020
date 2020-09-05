4fd9d1b60199c94e9dc212ef59f471f628852016
Urjeet Shrestha <42304832+ushrestha57@users.noreply.github.com>
2020-09-05 13:52:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const app = express(); //Now we can make routes
 
 const postRoutes = require('./routes/locationPost');
@@ -13,7 +14,38 @@ client.connect(err => {
   client.close();
 });
 
-app.use('/location',postRoutes)
+app.post('/users', async (req,res) => {
+    try{
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password,salt)
+        console.log(salt)
+        console.log(hashedPassword)
+        const user = {name: req.body.name, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch{
+        res.status(500).send()
+    }
+    
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+      return res.status(400).send('Cannot find user')
+    }
+    try {
+      if(await bcrypt.compare(req.body.password, user.password)) {
+        res.send('Success')
+      } else {
+        res.send('Not Allowed')
+      }
+    } catch {
+      res.status(500).send()
+    }
+  })
+
+
 
 
 app.listen(3000)
