29c3544bf3ee9584db3a7ffef2b61e09b13630b1
Joshua Askharoun <jjask7@gmail.com>
2020-09-06 06:52:25 -0500

    Created Xamarin.Forms projects

---
+++ HowdyHack2020/HowdyHack2020/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HowdyHack2020.Services;
+using HowdyHack2020.Views;
+
+namespace HowdyHack2020
+{
+	public partial class App : Application
+	{
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ HowdyHack2020/HowdyHack2020/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HowdyHack2020.Models;
+
+namespace HowdyHack2020.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
